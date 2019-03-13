73f69588d446503103165a0479ab75f3f17bd863
101teamA9 <48532796+101teamA9@users.noreply.github.com>
2019-03-13 20:27:26 +0000

    Add files via upload

---
+++ data exploratory.py
@@ -0,0 +1,211 @@
+#library
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+from matplotlib.pyplot import xticks
+
+
+#import data
+test = pd.read_csv(r"C:\Users\deya_\Desktop\university\ce101\test.csv")
+train = pd.read_csv(r"C:\Users\deya_\Desktop\university\ce101\train.csv")
+print(df_train.columns)
+
+train.head()
+
+train.columns
+
+
+#quantitative data
+quantitative = [f for f in df_train.columns if df_train.dtypes[f] != 'object']
+quantitative.remove('SalePrice')
+quantitative.remove('Id')
+qualitative = [f for f in df_train.columns if df_train.dtypes[f] == 'object']
+
+# %matplotlib inline
+#correlation matrix - relationship between variables
+corrmat = train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True);
+
+
+# related to SalePrice are
+# /!GarageCars - num
+# /!GarageArea - num/removed
+# /!TotRmsAbvGrd - num/removed
+# /!FullBath - num
+# /!!GrLivArea - num
+# /!1stFlrSF - num
+# /!TotalBsmtSF - num/removed
+# /!YearRemodAdd - num
+# /!YearBuilt - num
+# /!!OverallQual - num
+
+
+#saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+#scatterplot 1 and 2
+sns.set()
+cols = ['SalePrice', 'OverallQual', '1stFlrSF', 'YearRemodAdd', 'GrLivArea', 'GarageCars', 'FullBath', 'YearBuilt']
+sns.pairplot(train[cols], size = 2.5)
+plt.show();
+
+# UNIVARIATE ANALYSIS
+#standardizing data
+saleprice_scaled = StandardScaler().fit_transform(train['SalePrice'][:,np.newaxis]);
+low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
+high_range= saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('\nouter range (high) of the distribution:')
+print(high_range)
+
+#scatter plot 1stFlrSF/saleprice
+var = '1stFlrSF'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+#scatter plot GarageCars/saleprice
+var = 'GarageCars'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+#scatter plot GrLivArea/saleprice
+var = 'GrLivArea'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+#scatter plot FullBath/saleprice
+var = 'FullBath'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+#scatter plot YearBuilt/saleprice
+var = 'YearBuilt'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+#scatter plot OverallQual/saleprice
+var = 'OverallQual'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));#
+
+#scatter plot YearRemodAdd/saleprice
+var = 'YearRemodAdd'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+#box plot overallqual/saleprice
+var = 'OverallQual'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+
+#box plot YearRemodAdd/saleprice
+var = 'YearRemodAdd'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+
+#box plot FullBath/saleprice
+var = 'FullBath'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+
+#box plot Yearbuilt/saleprice
+var = 'YearBuilt'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+
+#box plot GarageCars/saleprice
+var = 'GarageCars'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+
+# transformation
+
+train.SalePrice = np.log(train.SalePrice)
+
+sns.distplot(train.SalePrice)
+
+
+#deleting points
+df_train.sort_values(by = 'GrLivArea', ascending = False)[:2]
+df_train = df_train.drop(df_train[df_train['Id'] == 1299].index)
+df_train = df_train.drop(df_train[df_train['Id'] == 524].index)
+
+#bivariate analysis saleprice/grlivarea
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+#histogram and normal probability plot
+sns.distplot(train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(train['SalePrice'], plot=plt)
+
+#applying log transformation
+df_train['SalePrice'] = np.log(df_train['SalePrice'])
+
+#histogram and normal probability plot
+sns.distplot(df_train['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['GrLivArea'], plot=plt)
+
+#data transformation
+train['GrLivArea'] = np.log(train['GrLivArea'])
+
+
+#transformed histogram and normal probability plot
+sns.distplot(df_train['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['GrLivArea'], plot=plt)
+
+
+#histogram and normal probability plot
+sns.distplot(df_train['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['TotalBsmtSF'], plot=plt)
+
+#create column for new variable (one is enough because it's a binary categorical feature)
+if area>0 it gets 1, for area==0 it gets 0
+df_train['HasBsmt'] = pd.Series(len(df_train['TotalBsmtSF']), index=df_train.index)
+df_train['HasBsmt'] = 0 
+df_train.loc[df_train['TotalBsmtSF']>0,'HasBsmt'] = 1
+
+#transform data
+train.loc[train['HasBsmt']==1,'TotalBsmtSF'] = np.log(train['TotalBsmtSF'])
+sns.set(rc={'figure.figsize':(25,8)})
+sns.countplot(train.YearBuilt)
+xticks(rotation=90)
+
+#histogram and normal probability plot
+sns.distplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], plot=plt)
+
+#scatter plot
+plt.scatter(train['GrLivArea'], train['SalePrice']);
+
+#scatter plot
+plt.scatter(train[train['TotalBsmtSF']>0]['TotalBsmtSF'], train[train['TotalBsmtSF']>0]['SalePrice']);
+
